c005f5b471b69d30f95c4a9bc90bddce43f809e3
Michael_Love <mike@michaeljlove.co.uk>
2021-02-15 17:18:13 +0000

    Test 4

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,8 +4,9 @@
  **/
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello Word!");
+        System.out.println("Hello World!");
     }
     //adding comment to check branching and merging
     //second comment here to check that merging from dev to test is working ok.
+    //Comment from Michael 3
 }
