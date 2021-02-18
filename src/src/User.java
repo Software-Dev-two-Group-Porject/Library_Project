ec07cd93c226b32613a02a581dace63aa0a73e07
Johnny <jmccauley117@gmail.com>
2021-02-18 10:33:10 +0000

    Create User
    Created User tocheck that merge is taking place successfully

---
+++ src/src/User.java
@@ -0,0 +1,15 @@
+/**
+ * Created By Jonathon on 18/02/2021
+ * Update Comments About Program Here
+ **/
+public class User {
+
+    //Creating User File to confirm merge from just created branch in dev branch
+    String id, firstname, surname;
+
+    User(String id, String firstname, String surname){
+     this.id = id;
+     this.firstname = firstname;
+     this.surname = surname; 
+    }
+}
