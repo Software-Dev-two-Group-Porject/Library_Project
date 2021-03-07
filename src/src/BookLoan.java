8cb5874edc6ced047e06f030410300edced7dbd2
RebeccaMcAvoy <mcavoy-r1@ulster.ac.uk>
2021-03-07 22:36:04 +0000

    getDaysOverDue Method
    Working on method to introduce the getDaysOverdue feature.

--- src/src/BookLoan.java
+++ src/src/BookLoan.java
@@ -1,6 +1,8 @@
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,7 +13,7 @@ import java.util.Date;
 public class BookLoan
 {
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yy hh:mm:ss");
-    LocalDate today = LocalDate.now();
+    LocalDate today = LocalDate.now(); //importing LocalDate Library
 
     //Declare Variables
     int isbn;
@@ -33,12 +35,6 @@ public class BookLoan
        this.isOverdue = isOverdue;
        this.finePerDay = finePerDay;
     }//constructor
-
-   //Possible Methods for this class
-   //calculateBookFine
-   //payFine - gives user option to pay fine now/or states their is no fine as book returned on time
-   //fineStatus (linked to above method?)
-   //dateDueBack / dateIssued
 
 
    //Get and Set Methods
@@ -84,8 +80,19 @@ public class BookLoan
 
    //instead of being a value within the constructor we can use the
    public int getDaysOverDue(){
-        
+      LocalDate nowDate = LocalDate.now(); //getting current date
+      LocalDate pastDate = ; //if storing dateReturned, requires change of data type to LocalDate for 'dateReturned' variable
+      return (int) ChronoUnit.DAYS.between(pastDate,nowDate); //return number of days difference
    }
+
+   //or - alternative method to above
+   ////instead of being a value within the constructor we can use the
+   //      public int getDaysOverDue(){
+   //         LocalDate nowDate = LocalDate.now(); //getting current date
+   //         LocalDate secondDate = LocalDate.parse(dateReturned,formatter);// correct variable use here? - //parse used to convert String to LocalDate format
+   //         long daysDifference = ChronoUnit.DAYS.between(nowDate,secondDate);
+   //         return (int) daysDifference;
+   //      }
 
 
    public void setOverdue(boolean overdue)
